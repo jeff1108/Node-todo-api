30b74032fcf389ffb3e8ea6fc695676627015c80
Hung Ka Kin <jeffhongkong2007@gmail.com>
2019-01-30 16:11:17 +0800

    Add update script

---
+++ playground/mongodb-update.js
@@ -0,0 +1,38 @@
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+
+  // db.collection('Todos').findOneAndUpdate({
+  //   _id: new ObjectID('5c5009948db71cf1ba9d0c00')
+  // }, {
+  //   $set: {
+  //     completed:  true
+  //   }
+  // }, {
+  //   returnOriginal: false
+  // }).then((result) => {
+  //   console.log(result);
+  // });
+
+  db.collection('Users').findOneAndUpdate({
+    _id: new ObjectID('5c4ad58d395aa556986652f3')
+  }, {
+    $set: {
+      name: 'Jeff'
+    },
+    $inc: {
+      age: 1
+    },
+  }, {
+    returnOriginal: false
+  }).then((results) => {
+    console.log(results);
+  });
+
+
+  // db.close();
+});
